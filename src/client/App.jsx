f0563de2bde22a18f086a5370d0dd3d3a929aaf2
yesrab <yesrab.ali111@gmail.com>
2024-07-03 23:07:51 +0530

    configured routes

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,9 +1,38 @@
+import { useContext } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import DashBoard, { loader as dashBoardLoader } from "./pages/dashboard/DashBoard";
+import Login, { action as loginAction } from "./pages/account/Login";
+import SignUp,{action as signUpAction} from "./pages/account/SignUp";
+import { LoginContext } from "./context/loginContext";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  const { loginState, dispatch } = useContext(LoginContext);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route
+          loader={({ request, params }) => dashBoardLoader({ loginState, request, params })}
+          index
+          element={<DashBoard />}
+        />
+        <Route>
+          <Route
+            path='/login'
+            action={({ request, params }) => loginAction({ dispatch, request, params })}
+            element={<Login />}
+          />
+          <Route path='/signup' action={({ request, params })=> signUpAction({ request, params })} element={<SignUp />} />
+        </Route>
+      </Route>,
+    ),
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
