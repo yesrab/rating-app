b65e3215d4f63ea76afcb315b03f1a8c05aa6855
yesrab <yesrab.ali111@gmail.com>
2024-07-03 23:22:43 +0530

    implemented toast package

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -7,8 +7,9 @@ import {
 } from "react-router-dom";
 import DashBoard, { loader as dashBoardLoader } from "./pages/dashboard/DashBoard";
 import Login, { action as loginAction } from "./pages/account/Login";
-import SignUp,{action as signUpAction} from "./pages/account/SignUp";
+import SignUp, { action as signUpAction } from "./pages/account/SignUp";
 import { LoginContext } from "./context/loginContext";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const { loginState, dispatch } = useContext(LoginContext);
@@ -26,13 +27,38 @@ function App() {
             action={({ request, params }) => loginAction({ dispatch, request, params })}
             element={<Login />}
           />
-          <Route path='/signup' action={({ request, params })=> signUpAction({ request, params })} element={<SignUp />} />
+          <Route
+            path='/signup'
+            action={({ request, params }) => signUpAction({ request, params })}
+            element={<SignUp />}
+          />
         </Route>
       </Route>,
     ),
   );
 
-  return <RouterProvider router={router} />;
+  useEffect(() => {
+    const toastId = toast("admin username : admin , passowrd : adminadmin");
+    return () => {
+      toast.dismiss(toastId);
+    };
+  }, []);
+
+  return (
+    <>
+      <Toaster
+        toastOptions={{
+          style: {
+            fontFamily: "CDM",
+            background: "#319aef",
+            boreder: "2px solid white",
+            color: "white",
+          },
+        }}
+      />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
