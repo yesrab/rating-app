2f43b8c554262e3ba0c40702ae63c380be1d29a7
yesrab <yesrab.ali111@gmail.com>
2024-07-04 16:31:16 +0530

    packages and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/client/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/client/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        loginGradient: "linear-gradient(96deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)"
+      backgroundImage: {
+        loginGradient: "linear-gradient(96deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)",
       },
-      fontFamily:{
-        cinacav:['CDM','Consolas']
-      }
+      colors: {
+        tgOrange: "#ef5429",
+      },
+      fontFamily: {
+        cinacav: ["CDM", "Consolas"],
+      },
     },
   },
   plugins: [],
-}
-
+};
